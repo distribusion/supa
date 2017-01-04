b188d4b45f9ae52e1a296548ed92cd4180dc1929
jrietema <jan.rietema@distribusion.com>
2017-01-04 16:59:34 +0100

    literal string or numeric as attribute getter

--- spec/models/commands/attribute_spec.rb
+++ spec/models/commands/attribute_spec.rb
@@ -1,23 +1,41 @@
 require 'spec_helper'
 
 describe Supa::Commands::Attribute do
+  let(:context) { OpenStruct.new(name: 'Zaphod') }
+  subject(:attribute) { Supa::Commands::Attribute.new(context: context, name: :name, tree: {}, options: options) }
+  let(:render_result) { attribute.represent }
+  let(:expected_name) { 'Zaphod' }
+
   describe '#initialize' do
     it '' do
     end
   end
 
   describe '#represent' do
-    it '' do
-    end
-  end
+    let(:options) { {} }
 
-  describe '#with_getter?' do
-    it '' do
-    end
+    it { expect(render_result).to eq(expected_name) }
   end
 
   describe '#getter' do
-    it '' do
+    context 'as proc' do
+      let(:options) { { getter: proc { self.name } } }
+
+      it { expect(render_result).to eq(expected_name) }
+    end
+
+    context 'as string literal' do
+      let(:options) { { getter: 'Bluejay' } }
+      let(:expected_name) { 'Bluejay' }
+
+      it { expect(render_result).to eq(expected_name) }
+    end
+
+    context 'as numeric literal' do
+      let(:options) { { getter: 1.75 } }
+      let(:expected_name) { 1.75 }
+
+      it { expect(render_result).to eq(expected_name) }
     end
   end
 end
