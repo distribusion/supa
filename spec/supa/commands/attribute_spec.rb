8c91dcaa7f7d355a888d7c796c974207be5a4b0a
mukimov.d <gigadamer@gmail.com>
2017-01-24 11:13:30 +0100

    Remove empty test files

--- spec/supa/commands/attribute_spec.rb
+++ spec/supa/commands/attribute_spec.rb
@@ -9,11 +9,6 @@ describe Supa::Commands::Attribute do
     described_class.new(object, representer: representer, tree: {}, name: :name, options: options)
   end
 
-  describe '#initialize' do
-    it '' do
-    end
-  end
-
   describe '#represent' do
     let(:options) { {} }
 
@@ -31,15 +26,6 @@ describe Supa::Commands::Attribute do
       let(:options) { {getter: :name_as_sym, modifier: :to_s} }
 
       it { expect(render_result).to eq('zaphod') }
-    end
-
-    context 'when pass an execution context' do
-      let(:options) { {getter: :mothod_name, exec_context: :representer} }
-
-      it do
-        pending('TBD')
-        raise
-      end
     end
   end
 end
--- spec/supa/version_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Supa::VERSION do
-  it 'has a version number' do
-    expect(::Supa::VERSION).not_to be_nil
-  end
-end
