693c0110e07c215e181b7d796000e3969674482b
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2017-02-27 17:29:34 +0100

    Make :modifier accept only symbols

--- lib/supa/command.rb
+++ lib/supa/command.rb
@@ -1,5 +1,7 @@
 module Supa
   class Command
+    UnsupportedModifier = Class.new(StandardError)
+
     def initialize(subject, representer:, tree:, name:, options: {}, &block)
       @subject = subject
       @representer = representer
@@ -21,7 +23,19 @@ module Supa
     end
 
     def modifier
-      @options[:modifier]
+      return @modifier if defined?(@modifier)
+      @modifier = @options[:modifier]
+      validate_modifier!
+      @modifier
+    end
+
+    def validate_modifier!
+      raise_unsupported_modifier_error if @modifier && !@modifier.is_a?(Symbol)
+    end
+
+    def raise_unsupported_modifier_error
+      raise UnsupportedModifier,
+        "Object #{@modifier.inspect} is not a valid modifier. Please provide symbolized method name."
     end
 
     def apply_render_flags(val)
--- spec/supa/commands/attribute/options_spec.rb
+++ spec/supa/commands/attribute/options_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Supa::Commands::Attribute, 'options' do
-  let(:object) { double('DummyArticle', name: 'Zaphod', full_name: 'Zaphod Surname', name_as_sym: :zaphod) }
+  let(:object) { double('DummyArticle', name: 'Zaphod', full_name: 'Zaphod Dohpaz', name_as_sym: :zaphod) }
   let(:representer) { Supa::ArticleRepresenter.new(object) }
   let(:attribute) do
     described_class.new(object, representer: representer, tree: {}, name: :name, options: options)
@@ -11,13 +11,46 @@ describe Supa::Commands::Attribute, 'options' do
   context 'when :getter option is given' do
     let(:options) { {getter: :full_name} }
 
-    it { expect(render_result).to eq('Zaphod Surname') }
+    it { expect(render_result).to eq('Zaphod Dohpaz') }
   end
 
   context 'when :getter and :modifier options are given' do
     let(:options) { {getter: :name_as_sym, modifier: :to_s} }
 
     it { expect(render_result).to eq('zaphod') }
+  end
+
+  context 'when :modifier option is a symbol (method name)' do
+    let(:attribute) do
+      described_class.new(object, representer: representer, tree: {}, name: :name_as_sym, options: options)
+    end
+
+    context 'when representer responds to given method name' do
+      let(:options) { {modifier: :to_s} }
+
+      it { expect(render_result).to eq('zaphod') }
+    end
+
+    context 'when representer does not respond to given method name' do
+      let(:options) { {modifier: :to_string} }
+
+      it 'raises error' do
+        expect { render_result }.to raise_error(
+          NoMethodError, /undefined method `to_string' for #<Supa::ArticleRepresenter/
+        )
+      end
+    end
+  end
+
+  context 'when :modifier option holds unsupported value type' do
+    let(:options) { {modifier: 'to_s'} }
+
+    it 'raises error' do
+      expect { render_result }.to raise_error(
+        Supa::Command::UnsupportedModifier,
+        'Object "to_s" is not a valid modifier. Please provide symbolized method name.'
+      )
+    end
   end
 
   context 'when :hide_when_nil option is set to true' do
