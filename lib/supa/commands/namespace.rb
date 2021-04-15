d4519738cc54d2ee2ed6ad75cdd48fb8e25d762d
MiWieczo <dzikimiki93@gmail.com>
2021-04-15 17:27:40 +0200

    make namespace hidable

--- lib/supa/commands/namespace.rb
+++ lib/supa/commands/namespace.rb
@@ -2,9 +2,18 @@ module Supa
   module Commands
     class Namespace < Supa::Command
       def represent
+        return if hide?
+
         @tree[@name] = {}
+        return if value.nil? && empty_when_nil?
 
         Supa::Builder.new(@subject, representer: @representer, tree: @tree[@name]).instance_exec(&@block)
+      end
+
+      private
+
+      def hide?
+        (value.nil? || value.empty?) && hide_when_empty?
       end
     end
   end
